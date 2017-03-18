17e8f05bcbad9ded17e199e61c63e7b1124945d4
Gunnar <gpeipman@hotmail.com>
2017-03-18 14:27:28 +0200

    initial commit

---
+++ AspNetCoreChatRoom/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreChatRoom.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("InsertUserName");
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            return View("Index", username);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
